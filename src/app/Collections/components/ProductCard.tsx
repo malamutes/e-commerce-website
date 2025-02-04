acb9b1ca6bbf154212c63ecda433afc365e9cf62
malamutes <hehexdroflmfao@gmail.com>
2025-02-04 20:45:23 +0800

    Product Card Sales Cat UI Update

--- src/app/Collections/components/ProductCard.tsx
+++ src/app/Collections/components/ProductCard.tsx
@@ -5,7 +5,22 @@ import { faTrophy, faCrown, faTags, IconDefinition, faBoxOpen } from "@fortaweso
 import Link from "next/link";
 import WishlistBookmark from "@/app/components/WishlistBookmark";
 import { ProductCardInterface } from "@/app/DataInterfaces";
-import { SetStateAction } from "react";
+import { SetStateAction, useState } from "react";
+
+//for now just assume the image aspect ratio is 4:3 and in db it is 800:600px
+const productSalesCategoryMap: { [key: string]: string } = {
+    'Exclusive': 'rgba(211, 183, 23, 0.75)',
+    'Best Sellers': 'rgba(0, 128, 0, 0.75)',
+    'Sale': 'rgba(255, 0, 0, 0.75)',
+    'New Arrivals': 'rgba(0, 255, 255, 0.75)'
+};
+
+const productSalesIconMap: { [key: string]: IconDefinition } = {
+    'Exclusive': faCrown,
+    'Best Sellers': faTrophy,
+    'Sale': faTags,
+    'New Arrivals': faBoxOpen
+}
 
 interface ProductCard {
     product: ProductCardInterface,
@@ -15,19 +30,47 @@ interface ProductCard {
     seachBarQuery?: React.Dispatch<SetStateAction<string>>
 }
 
-//for now just assume the image aspect ratio is 4:3 and in db it is 800:600px
-const productSalesCategoryMap: { [key: string]: string } = {
-    'Exclusive': 'gold',
-    'Best Sellers': 'green',
-    'Sale': 'red',
-    'New Arrivals': 'cyan'
+interface ProductCardSalesCategory {
+    category: string
 }
 
-const productSalesIconMap: { [key: string]: IconDefinition } = {
-    'Exclusive': faCrown,
-    'Best Sellers': faTrophy,
-    'Sale': faTags,
-    'New Arrivals': faBoxOpen
+function ProductCardSalesCategory(props: ProductCardSalesCategory) {
+
+    const [showCategoryName, setShowCategoryName] = useState(false);
+
+    return (
+        <div className="relative w-fit"
+            onMouseLeave={() => setShowCategoryName(false)}
+            onMouseEnter={() => setShowCategoryName(true)}
+        >
+            <div
+                className="p-2 grid place-items-center rounded-full"
+                style={{
+                    background:
+                        `${productSalesCategoryMap[props.category]}`,
+                    backdropFilter: "blur(10px)",
+                    WebkitBackdropFilter: "blur(10px)", // For Safari
+                    border: "1px solid rgba(255, 255, 255, 0.2)",
+                    borderRadius: "50%",
+                    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"
+                }}>
+                {
+                    productSalesIconMap[props.category]
+                        ? (<FontAwesomeIcon
+                            className="3xs:text-[20px] text-[14px]"
+                            icon={productSalesIconMap[props.category]} />)
+                        : (null)
+                }
+            </div >
+            <div
+                className={`absolute top-full left-0 w-fit bg-gray-600 text-white px-2 py-1 rounded left-1/2 -translate-x-1/2
+                transition-opacity duration-300 mt-[5px]
+                ${showCategoryName ? "opacity-100" : "opacity-0 pointer-events-none"} whitespace-nowrap`}
+            >
+                {props.category}
+            </div>
+        </div>
+    )
 }
 
 export default function ProductCard(props: ProductCard) {
@@ -55,11 +98,11 @@ export default function ProductCard(props: ProductCard) {
                     />
 
                     <div className="flex flex-col bg-gray-400 pb-5 ">
-                        <span className="mt-2 italic text-sm">
+                        <span className="mt-2 italic text-sm  break-all">
                             {props.product.product_producer}
                         </span>
 
-                        <span className="font-bold text-sm ">
+                        <span className="font-bold text-sm break-all">
                             {props.product.product_name}
                         </span>
 
@@ -67,28 +110,15 @@ export default function ProductCard(props: ProductCard) {
                             ${props.product.product_price}
                         </span>
 
-                        <div className={`flex sm:flex-row flex-col mx-auto ${props.showTags === false ? "hidden" : ""}`}>
+                        <div className={`2xs:w-full 2xs:flex 2xs:flex-row 2xs:justify-center grid grid-cols-2 justify-items-center
+                             mx-auto 3xs:gap-2 gap-1.5 ${props.showTags === false ? "hidden" : ""}`}>
                             {(props.product.product_sales_category ?? [])
                                 .filter(cat => cat !== 'Regular')
                                 .map((category, index) => (
-                                    <div key={index}
-                                        className="p-1.5 flex items-center"
-                                        style={{
-                                            backgroundColor:
-                                                `${productSalesCategoryMap[category]}`
-                                        }}>
-
-                                        <span className="text-sm">
-                                            {category}
-                                        </span>
-
-
-                                        {productSalesIconMap[category]
-                                            ? (<FontAwesomeIcon
-                                                className="ml-2 text-xs"
-                                                icon={productSalesIconMap[category]} />)
-                                            : (null)}
-                                    </div>
+                                    <ProductCardSalesCategory
+                                        category={category}
+                                        key={index}
+                                    />
                                 ))}
                         </div>
                     </div>
