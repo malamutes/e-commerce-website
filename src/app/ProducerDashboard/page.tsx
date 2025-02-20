8510569bdcf7b43722b781f462305d79378d404c
malamutes <hehexdroflmfao@gmail.com>
2025-02-20 17:51:25 +0800

    s4v

--- src/app/ProducerDashboard/page.tsx
+++ src/app/ProducerDashboard/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Suspense } from "react";
-import ProducerDashboard from "./ProducerDashboard";
 
 export default function ProductDashboardPage() {
     return (
         <Suspense>
-            <ProducerDashboard />
+            <span>
+                hello
+            </span>
         </Suspense>
     )
 }
