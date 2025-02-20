e412d8aeb753a8e4e627d3437b20312de7a63f1c
malamutes <hehexdroflmfao@gmail.com>
2025-02-20 18:38:43 +0800

    seo update 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="robots" content="index, follow" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
